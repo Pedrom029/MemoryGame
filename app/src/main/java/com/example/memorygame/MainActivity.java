1988101066d818957e62d15eb77614e03d3623d8
Pedrom029 <pedrom029@gmail.com>
2019-11-29 18:49:41 +0000

    Adicionadas coisas novas: - Activity do jodo - Activity do score - Banner de publicidade no fundo da pagina do score

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.memorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    public void onClickGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickScore(View view) {
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/memorygame/ScoreActivity.java
@@ -0,0 +1,25 @@
+package com.example.memorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+    }
+}
